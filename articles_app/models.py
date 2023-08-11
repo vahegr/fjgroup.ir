802634a4d01d00daade95d758e61d11666aa1e01
vahegr <vahegrigorian447@gmail.com>
2023-08-11 12:41:28 +0330

    article model created

--- articles_app/models.py
+++ articles_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+# model writen by two type of language
+class Article(models.Model):
+    image = models.ImageField(upload_to='images/article_images')
+
+    title = models.CharField(max_length=300)
+    persian_title = models.CharField(max_length=300)
+
+    description = models.TextField(max_length=1500)
+    persian_description = models.TextField(max_length=1500)
+
+    pdf_file = models.FileField(upload_to='pdfs/article_pdf')
+
+    def __str__(self):
+        return f'{self.title} - {self.description[:30]}...'
+
+    class Meta:
+        verbose_name = 'مقاله'
+        verbose_name_plural = 'مقالات'
