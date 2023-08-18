194ab2025ef105601f1dedb896f5ebeafb068737
vahegr <vahegrigorian447@gmail.com>
2023-08-18 13:55:59 +0330

    contact information model created

--- contact_app/models.py
+++ contact_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ContactInformation(models.Model):
+    phone = models.CharField(max_length=20)
+    second_phone = models.CharField(max_length=20)
+    third_phone = models.CharField(max_length=20)
+    location = models.CharField(max_length=1200)
+    email = models.EmailField(max_length=300)
+
+    def __str__(self):
+        return f'{self.phone} - {self.email} - {self.location}'
+
+    class Meta:
+        verbose_name = 'طلاعات تماس'
+        verbose_name_plural = 'اطلاعات تماس'
+
