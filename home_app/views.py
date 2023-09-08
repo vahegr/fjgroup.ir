c0ff6c357dd79dda39f750907604b96fdffdc9aa
vahegr <vahegrigorian447@gmail.com>
2023-09-09 01:41:10 +0330

    home page completed

--- home_app/views.py
+++ home_app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from projects_app.models import Project
 
-# Create your views here.
+
+def home_view(request):
+    recent_projects = Project.objects.all()[:20]
+    return render(request, 'home_app/index.html', context={'projects': recent_projects})
