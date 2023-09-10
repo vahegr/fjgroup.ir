e8e6aab264a6c18f0d73dedcbd09add27f4591b2
vahegr <vahegrigorian447@gmail.com>
2023-09-10 18:22:55 +0330

    team member model created

--- about_app/models.py
+++ about_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class TeamMember(models.Model):
+    image = models.ImageField(upload_to='images/team_members_images', verbose_name='عکس')
+    full_name = models.CharField(max_length=300, verbose_name='نام و نام خانوادگی')
+    persian_full_name = models.CharField(max_length=300, verbose_name='نام و نام خانوادگی(فارسی)')
+    bio = models.CharField(max_length=500, verbose_name='بایو')
+    persian_bio = models.CharField(max_length=500, verbose_name='بایو(فارسی)')
+    pdf_file = models.FileField(upload_to='pdfs/article_pdf', verbose_name='فایل')
+
+    def __str__(self):
+        return f'{self.full_name} - {self.bio[:30]}...'
+
+    class Meta:
+        verbose_name = 'عضو تیم'
+        verbose_name_plural = 'افراد تیم'
